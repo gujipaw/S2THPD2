3e1c240b86e8aebf0796e83ab6787d9bb5640299
gujipaw <82021250+gujipaw@users.noreply.github.com>
2021-04-14 11:43:05 +0200

    Update exo_03.rb

--- exo_03.rb
+++ exo_03.rb
@@ -1,5 +1,5 @@
 puts "Quel est ton année de naissance?"
-user_input_year = gets.chomp
+user_input_year = gets.chomp.to_i
 current_year = Time.now.strftime("%Y").to_i
 specific_year = #{current_year - user_input_year}
 puts "Bonjour, #{user_input_year} est mon année de naissance! On est en #{current_year}." 
